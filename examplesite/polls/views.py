bd19cd6eba56dc9395097082ffb2180d5621230a
Boney Patel <boneypatel@boneys-air.mynetworksettings.com>
2023-03-19 13:37:56 -0400

    Add test cases

--- examplesite/polls/views.py
+++ examplesite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Question, Choice
 # Create your views here.
@@ -11,17 +12,26 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
